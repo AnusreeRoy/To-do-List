1d55fcdb139d253735b3ca108499ccf1132015b0
midnightGlow <119887004+AnusreeRoy@users.noreply.github.com>
2025-02-24 01:48:23 +0600

    Update app.py

--- app.py
+++ app.py
@@ -63,4 +63,6 @@ def update_task(task_id):
         
         
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get port from Railway
+    app.run(host="0.0.0.0", port=port)
